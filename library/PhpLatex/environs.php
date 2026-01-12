64b18e5c883b8ed197f91ac07ac1b10e18d5864c
Lars Willighagen <lars.willighagen@gmail.com>
2026-01-12 22:14:15 +0100

    Add numOptArgs to environs

--- library/PhpLatex/environs.php
+++ library/PhpLatex/environs.php
@@ -2,62 +2,65 @@
 
 return array(
     'verbatim'    => array(
-        'verbatim'  => true,
-        'mode'      => PhpLatex_Parser::MODE_TEXT,
-        'environs'  => array('itemize', 'enumerate'),
-        'starred'   => true,
+        'verbatim'   => true,
+        'mode'       => PhpLatex_Parser::MODE_TEXT,
+        'environs'   => array('itemize', 'enumerate'),
+        'starred'    => true,
         // verbatim in tabular causes
         // ! LaTeX Error: Something's wrong--perhaps a missing \item.
     ),
     'Verbatim'    => array(
-        'verbatim'  => true,
-        'mode'      => PhpLatex_Parser::MODE_TEXT,
-        'environs'  => array('itemize', 'enumerate'),
+        'verbatim'   => true,
+        'mode'       => PhpLatex_Parser::MODE_TEXT,
+        'environs'   => array('itemize', 'enumerate'),
     ),
     'lstlisting'  => array(
-        'verbatim'  => true,
-        'mode'      => PhpLatex_Parser::MODE_TEXT,
-        'environs'  => array('itemize', 'enumerate'),
+        'numOptArgs' => 1,
+        'verbatim'   => true,
+        'mode'       => PhpLatex_Parser::MODE_TEXT,
+        'environs'   => array('itemize', 'enumerate'),
     ),
     'enumerate'   => array(
-        'mode'      => PhpLatex_Parser::MODE_TEXT,
-        'environs'  => array('itemize', 'enumerate'),
+        'mode'       => PhpLatex_Parser::MODE_TEXT,
+        'environs'   => array('itemize', 'enumerate'),
     ),
     'itemize'     => array(
-        'mode'      => PhpLatex_Parser::MODE_TEXT,
-        'environs'  => array('itemize', 'enumerate'),
+        'numOptArgs' => 1,
+        'mode'       => PhpLatex_Parser::MODE_TEXT,
+        'environs'   => array('itemize', 'enumerate'),
         // itemize in tabular causes
         // ! LaTeX Error: Something's wrong--perhaps a missing \item.
     ),
     'displaymath' => array(
-        'math'      => true,
-        'mode'      => PhpLatex_Parser::MODE_TEXT,
-        'environs'  => array('itemize', 'enumerate'),
+        'math'       => true,
+        'mode'       => PhpLatex_Parser::MODE_TEXT,
+        'environs'   => array('itemize', 'enumerate'),
         // displaymath in tabular causes
         // ! LaTeX Error: Bad math environment delimiter.
     ),
     'math'        => array(
-        'math'      => true,
-        'mode'      => PhpLatex_Parser::MODE_TEXT,
-        'environs'  => array('itemize', 'enumerate', 'tabular'),
+        'math'       => true,
+        'mode'       => PhpLatex_Parser::MODE_TEXT,
+        'environs'   => array('itemize', 'enumerate', 'tabular'),
     ),
     'equation' => array(
-        'mode'      => PhpLatex_Parser::MODE_TEXT,
-        'math'      => true,
-        'starred'   => true,
+        'mode'       => PhpLatex_Parser::MODE_TEXT,
+        'math'       => true,
+        'starred'    => true,
     ),
     'eqnarray' => array(
-        'mode'      => PhpLatex_Parser::MODE_TEXT,
-        'math'      => true,
-        'starred'   => true,
+        'mode'       => PhpLatex_Parser::MODE_TEXT,
+        'math'       => true,
+        'starred'    => true,
     ),
     'tabular' => array(
-        'numArgs'   => 1,
-        'mode'      => PhpLatex_Parser::MODE_TEXT,
-        'environs'  => array('itemize', 'enumerate', 'tabular'),
+        'numArgs'    => 1,
+        'numOptArgs' => 1,
+        'mode'       => PhpLatex_Parser::MODE_TEXT,
+        'environs'   => array('itemize', 'enumerate', 'tabular'),
     ),
     'array' => array(
-        'numArgs'   => 1,
-        'mode'      => PhpLatex_Parser::MODE_MATH,
+        'numArgs'    => 1,
+        'mode'       => PhpLatex_Parser::MODE_MATH,
     ),
 );
