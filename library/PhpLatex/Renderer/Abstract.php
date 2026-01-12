dd8356853c970d829b141ba43a8c2f4ccb9d59ee
Lars Willighagen <lars.willighagen@gmail.com>
2026-01-12 23:25:25 +0100

    Allow 'parseArgs' and 'numOptArgs' in environments (#14)
    Resolves #13

--- library/PhpLatex/Renderer/Abstract.php
+++ library/PhpLatex/Renderer/Abstract.php
@@ -97,7 +97,7 @@ abstract class PhpLatex_Renderer_Abstract
                     $argsEnd = 0;
 
                     foreach ($children as $child) {
-                        if ($child->arg) {
+                        if ($child->arg || $child->optional) {
                             ++$argsEnd;
                         } else {
                             break;
